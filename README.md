6c1ffbcbf4a7f977d8b4619dfb662d118d59b8af
Jacob Parker <jacob@solidangle.ca>
2019-07-13 22:03:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Titlist
+
+Reads the `<title>` from a URL and prints it to stdout.
+
+This is used as part of an IRC bot.
---
+++ src/main.rs
@@ -0,0 +1,80 @@
+extern crate html5ever;
+extern crate reqwest;
+
+use html5ever::tendril::*;
+use html5ever::local_name;
+use html5ever::tokenizer::{BufferQueue, CharacterTokens, TagToken, Token, TokenSink, TokenSinkResult, Tokenizer, StartTag, TokenizerOpts};
+use reqwest::Url;
+use std::process::exit;
+
+struct TitleFinder {
+    this_is_it: bool,
+}
+
+fn clean_up_title(title: &str) -> &str {
+    title.trim()
+}
+
+impl TokenSink for TitleFinder {
+    type Handle = ();
+
+    fn process_token(&mut self, token: Token, _: u64) -> TokenSinkResult<()> {
+        match token {
+            // Perk up for <title>
+            TagToken(tag) =>
+                if tag.kind == StartTag && tag.name == local_name!("title") {
+                    self.this_is_it = true;
+                }
+
+            // After <title> write out the string
+            CharacterTokens(txt) => {
+                if self.this_is_it {
+                    print!("{}\n", clean_up_title(&txt));
+                    exit(0);
+                }
+            },
+
+            // Ignore everything else
+            _ => (),
+        }
+
+        TokenSinkResult::Continue
+    }
+}
+
+fn main() {
+    let url = match std::env::args().nth(1) {
+        Some(url) => url,
+        None => exit(1),
+    };
+
+    let url = match Url::parse(&url) {
+        Ok(url) => url,
+        _ => exit(1),
+    };
+
+    let mut res = match reqwest::get(url) {
+        Ok(res) => res,
+        _ => exit(1),
+    };
+
+    let body = match res.text() {
+        Ok(body) => body,
+        _ => exit(1),
+    };
+
+    let tendril = ByteTendril::from_slice(body.as_bytes());
+
+    let mut input = BufferQueue::new();
+    input.push_back(tendril.try_reinterpret().unwrap());
+
+    let mut tokenizer = Tokenizer::new(
+        TitleFinder{this_is_it: false},
+        TokenizerOpts{..Default::default()},
+    );
+
+    let _ = tokenizer.feed(&mut input);
+
+    // If we get here we didn't find a <title>
+    exit(1);
+}
